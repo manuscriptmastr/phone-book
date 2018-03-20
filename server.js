02ff9a722a46ddecbe3293fe92fda51b6fd3125b
Joshua Martin <joshua@bluefishds.com>
2018-03-20 15:56:32 -0400

    Set up super generic router

--- server.js
+++ server.js
@@ -1,7 +1,12 @@
 const http = require('http');
+const phoneBook = require('./storage').phoneBook;
 
 const server = http.createServer((req, res) => {
-  
+  if (req.url === '/') {
+    res.end('Hello there!');
+  } else {
+    res.end('You asked for ' + req.url);
+  }
 });
 
 server.listen(3000);
--- storage.js
+++ storage.js
@@ -3,35 +3,35 @@ const promisify = require('util').promisify;
 const readFile = promisify(fs.readFile);
 const writeFile = promisify(fs.writeFile);
 
-var storage = fileName => {
+let storage = fileName => {
 
-  var generateId = () => Math.floor(Math.random() * 100**10);
+  let generateId = () => Math.floor(Math.random() * 100**10);
 
-  var get = () => {
-    var contents = readFile(fileName);
+  let get = () => {
+    let contents = readFile(fileName);
 
     return contents
       .then(data => JSON.parse(data))
       .catch(err => Promise.resolve([]));
   }
 
-  var save = (arr) => {
-    var contents = JSON.stringify(arr);
+  let save = (arr) => {
+    let contents = JSON.stringify(arr);
 
     return writeFile(fileName, contents);
   }
 
-  var add = obj => {
+  let add = obj => {
     obj.id = generateId();
-    var arr = get();
+    let arr = get();
 
     return arr
       .then(a => a.concat([obj]))
       .then(a => save(a));
   }
 
-  var remove = (id, arr) => {
-    var arr = get();
+  let remove = (id) => {
+    let arr = get();
 
     return arr
       .then(a => a.filter(obj => obj.id !== id))
@@ -46,4 +46,8 @@ var storage = fileName => {
 
 }
 
-module.exports = storage;
+let phoneBook = storage('phonebook.json');
+
+module.exports = {
+  phoneBook
+};
